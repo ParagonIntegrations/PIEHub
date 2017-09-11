cb56fc46d499e883dc186c6973d1abda35e3cc68
Nelis <nelisvolschenk@gmail.com>
2017-09-11 20:59:18 +0200

    Initial writing of program

--- Main.py
+++ Main.py
@@ -1,3 +1,22 @@
 #! /usr/bin/env python3
 
 import multiprocessing
+import time
+
+
+class HubManager(multiprocessing.Process):
+
+    def __init__(self):
+        multiprocessing.Process.__init__(self)
+
+    def run(self):
+
+        # Main Loop
+        while True:
+
+            time.sleep(1)
+
+if __name__ == "__main__":
+    hubman = HubManager()
+    hubman.start()
+    hubman.join()
