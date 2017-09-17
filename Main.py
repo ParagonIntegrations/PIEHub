9934b5d0a0cd6994bc98d37c0dd037b426fb2a95
Nelis <nelisvolschenk@gmail.com>
2017-09-17 13:45:15 +0200

    Initial writing of program

--- Main.py
+++ Main.py
@@ -2,6 +2,9 @@
 
 import multiprocessing
 import time
+import sqlite3
+
+
 
 
 class HubManager(multiprocessing.Process):
@@ -9,10 +12,23 @@ class HubManager(multiprocessing.Process):
     def __init__(self):
         multiprocessing.Process.__init__(self)
 
+
     def run(self):
+
+        # Start subprocesses
 
         # Main Loop
         while True:
+
+            # Check for new data from PIEmon
+
+            # Update Energy variables
+
+            # Send data to Database
+
+            # Send updated information to PIEmon every 60 mins
+
+
 
             time.sleep(1)
 
@@ -20,3 +36,11 @@ if __name__ == "__main__":
     hubman = HubManager()
     hubman.start()
     hubman.join()
+
+
+"""
+Things to do:
+
+
+
+"""
